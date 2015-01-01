4951ad74efc98b34f82e3a6c8a8e5c62e2ee7097
Courtney Buras <cburas2311@gmail.com>
2014-12-31 18:03:51 -0600

    Create file for main ruby code

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,18 @@
+class TodoList
+
+	def initialize(items)
+		items.to_a
+	end
+
+	def get_items(items)
+		items.to_a
+	end
+
+	def add_item
+	end
+	
+end
+
+# list = TodoList.new(["do the dishes", "mow the lawn"])
+
+# p list
