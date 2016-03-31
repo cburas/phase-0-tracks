7ea44709d8604190f5d2fef01ac139222960fafe
Courtney Buras <cburas2311@gmail.com>
2016-03-31 16:05:05 -0500

    Add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,13 @@
+# Enter client's name
+# Enter client's price range
+# Client's decor theme
+# Number of rooms
+# Willing to remodel?
+
+# - prompt designer for all info
+# - convert any input to the appropriate data
+# - print hash when all questions answered (at end)
+# - give opportunity  to update key
+# - if input 'none' then skip it
+# - person types "decor_theme" program should know it means :decor_theme
+# - print latest version and exit
