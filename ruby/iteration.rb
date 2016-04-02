66c129ce9222208699fe0c157bb2663f8fea5a7e
Courtney Buras <cburas2311@gmail.com>
2016-04-02 17:11:56 -0500

    Finish assignment with conclusion of release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -41,4 +41,48 @@ end
 
 p hash1
 
+array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p array2
+array2.delete_if{|item| item == 2}
+p array2
+
+hash2 = {
+  1 => "a",
+  2 => "b",
+  3 => "c",
+  4 => "d",
+  5 => "e",
+  6 => "f",
+  7 => "g",
+  8 => "h"
+  }
+hash2.delete_if{|number, letter| number <= 2}
+p hash2
+
+#Release 2 question 2
+array2.keep_if{|item| item <= 4}
+p array2
+
+hash2.keep_if {|number, letter| number >= 5 }
+p hash2
+
+#Release 2 question 3
+
+p array2.include?(3)
+p array2.index{|number| number == 1}
+
+p hash1.each_pair {|movie, actor| puts "#{actor} is in #{movie}"}
+
+#Release 2 question 4
+array2.push(5,6,7)
+array2 = array2.take_while {|number| number < 5}
+p array2
+
+p hash2
+hash2.reject!{|numbers, letters| numbers < 6}
+p hash2
+
+
+
+
  
