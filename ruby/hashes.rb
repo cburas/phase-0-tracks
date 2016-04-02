400cbc955c61cd9d04ec69cf4ea641df320b7552
Courtney Buras <cburas2311@gmail.com>
2016-04-02 14:31:28 -0500

    Make update section work

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,61 +1,54 @@
 # | prompt designer for all info
 # | convert any input to the appropriate data
 # | print hash when all questions answered (at end)
-# - give opportunity  to update key
-# - if input 'none' then skip it
+# | give opportunity  to update key
+# | if input 'none' then skip it
 # - person types "decor_theme" program should know it means :decor_theme
-# - print latest version and exit
+# | print latest version and exit
 
-Client = {}
+#set up hash
+client = {}
 
 # Enter client's name
 puts "What is the client's name?"
-name = gets.chomp
+client[:name] = gets.chomp
 
 # Enter client's price range
 puts "What is the client's price range?"
-price_range = gets.chomp
-price_range.to_i
+client[:price_range] = gets.chomp.to_i
 
 # Client's decor theme
 puts "What is the client's decor theme?"
-decor_theme = gets.chomp
+client[:decor_theme] = gets.chomp
 
 # Willing to remodel?
 puts "The client is willing to remodel? (true/false)"
-will_remodel = gets.chomp
+client[:will_remodel] = gets.chomp
 
-  puts "Any updates? (yes/none)"
-  update = gets.chomp
+ puts "Any updates? (yes/none)"
+ update = gets.chomp
+
     if update == "none"
-      p Client
+      p client
     else
     	puts "What is the section?"
-			section = gets.chomp
-			section.to_sym
+			section = gets.chomp.to_sym
 
-			puts "Type in the update."
-			update = gets.chomp
+		 	puts "Type in the update."
+		 	updates = gets.chomp
+		end
 
-			  if section == :price_range
-			    update.to_i
-			  end
+		 if section == :price_range
+			client[:price_range]  = "#{updates}"
+		 	updates = update.to_i
+		 elsif section == :will_remodel
+		 		client[:will_remodel] = "#{updates}"
+		 elsif section == :decor_theme
+		 	client[:decor_theme] = "#{updates}"
+		 elsif section == :name
+		 	client[:name] = "#{updates}"
+		 else
+		 end
 
-			  if update == "yes"
-			    :will_remodel == true
-			  elsif update == "no"
-			    :will_remodel == false
-			  else
-			  end
-			 end
-			 
 
-
-Client = { 
-  name: name,
-  price_range: price_range,
-  decor_theme: decor_theme,
-  will_remodel: will_remodel,
-}
-
-p Client
+p client
