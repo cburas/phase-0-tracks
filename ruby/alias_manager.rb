e4a10b0af95412e0c4298f2670503510ff619bcd
Courtney Buras <cburas2311@gmail.com>
2016-04-03 22:37:02 -0500

    Add pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,18 @@
+# - take spy's real name
+# - change all vowels (a,e,i,o,u) to next vowel in a,e,i,o,u
+# - change all consonants to next consonant in alphabet
+# - make program loop until user types 'quit'
+# - save names into an "appropriate" data structure (prob hash)
+# - iterate through data n print all when user quits.
+# - define a method w/ parameters
+
+# set vowels to variable
+# ask spy's real name and make downcase
+# make into array(split) and reverse words
+# separate the words by .chars
+# then use map! to affect each letter
+# still inside map!,use if statement to change vowels to next
+# use else to change all consonants to next
+# make exception for 'z' to become 'b'
+# gathering all names/data
+# puts " 'fake name' is actually 'real name' "
