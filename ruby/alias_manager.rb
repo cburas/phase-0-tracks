b57563ef9e26489430ca6d59ee72079f6dd97973
Courtney Buras <cburas2311@gmail.com>
2016-04-05 18:12:34 -0500

    Commit what I have

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,4 +1,4 @@
-# - take spy's real name
+# | take spy's real name
 # - change all vowels (a,e,i,o,u) to next vowel in a,e,i,o,u
 # - change all consonants to next consonant in alphabet
 # - make program loop until user types 'quit'
@@ -6,48 +6,37 @@
 # - iterate through data n print all when user quits.
 # - define a method w/ parameters
 
-# set vowels to variable
-# ask spy's real name and make downcase
-# make into array(split) and reverse words
-# separate the words by .chars
-# then use map! to affect each letter
+
 # still inside map!,use if statement to change vowels to next
 # use else to change all consonants to next
 # make exception for 'z' to become 'b'
-# gathering all names/data
 # puts " 'fake name' is actually 'real name' "
-
+# set vowels to variable
 vowels = ['a', 'e', 'i', 'o', 'u']
 
+# ask spy's real name and make downcase
 puts "What is spy's real name?"
 real_name = gets.chomp.downcase
 
 p real_name
-
+# make into array by chars
 real_array = real_name.chars
 
 p real_array
 
+# reverse! reverse!
 real_array.reverse!
 
 p real_array
 
-
-
+# then use map! to affect each letter
 fake_array = real_array.map! { |letter| 
-	if letter == (vowels[0] || vowels[1] || vowels[2] || vowels[3] || vowels[4])
-		vowels.next
-	elsif letter == 'z'
-		letter == 'b'
-	elsif letter == ' '
-		letter == '/'
-	else
-		letter.next
+	
 	end
 }
 
 p fake_array
-
+# gathering all names/data
 fake_name = fake_array.join
 
 p fake_name
