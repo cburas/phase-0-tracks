d72ae8f7d02e361e7f15fcbd6d57f48c35f11e39
Courtney Buras <cburas2311@gmail.com>
2016-04-06 14:25:39 -0500

    Add methods

---
+++ ruby/santa.rb
@@ -0,0 +1,15 @@
+class Santa
+
+	def initialize
+		p "initializing Santa instance.."
+	end
+
+	def speak
+		print "Ho, Ho, ho! Haaaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		p "That was a good #{cookie}!"
+	end
+
+end
