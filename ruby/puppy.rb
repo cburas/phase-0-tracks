4c1e9c55b4e6d8a61601cd643812adc2885fbfd4
Courtney Buras <cburas2311@gmail.com>
2016-04-05 22:10:41 -0500

    Pseudocode methods and attributes

---
+++ ruby/puppy.rb
@@ -0,0 +1,24 @@
+# class Puppy
+
+#attributes:
+	# fur? varies
+	# fur_color? varies
+	# size tiny, small, medium, large, giant
+	# age (should this be human or dog or both?)
+	# friendly? true/false
+	# friendly_with? people, kids, dogs, cats, strangers, 
+	# shed?
+	# maintenance? low, avg, high
+	# to_train? easy, avg, difficult
+
+#methods
+	# bark
+	# wag_tail
+	# sit
+	# lay
+	# play_dead
+	# stay
+	# come
+	# fetch
+	# let_go
+	# catch
