a5faf3be544a78fdc1b71ef5557d75d2349909fc
Courtney Buras <cburas2311@gmail.com>
2016-04-07 17:41:29 -0500

    Add attr, driver code still failing

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,12 +2,14 @@
 
 class Santa
 
-	def initialize  
+	attr_reader :gender :ethnicity
+	attr_accessor :reindeer_ranking
+
+	def initialize(gender, ethnicity) 
 		@gender  
 		@ethnicity  
 		@age = 0  
-		@reindeer_ranking =["Rudolph","Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid",
-	"Donner", "Blitzen"]  end
+		@reindeer_ranking =["Rudolph","Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]  end
 
 	def speak 
 		print "Ho, Ho, ho! Haaaaappy holidays!" 
@@ -17,17 +19,23 @@ class Santa
 		print "That was a good #{cookie}!" 
 	end
 
+	def celebrate_birthday
+		@age + 1
+	end
+
 end
 
 #Add some diverse initializations to file as driver code. 
 santas = []
 example_genders = ["agender", "female", "male", "transfemale", "transmale", "bigender", "gender fluid", "N/A"]
 
-example_ethnicities = ["white", "black", "Asian", "Indian", "Latino", "prefer not to say"]
+example_ethnicities = ["white", "black", "Asian", "Indian", "Latino", "prefer not to say", "African", "Middle Eastern"]
 
 example_genders.length.times do |i|
-	santas << Santa.new(example_ethnicities[i], example_genders[i])
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
+
+
 
 
 
