4156d6d216d798d14121c5f532afe74ace411c17
Courtney Buras <cburas2311@gmail.com>
2016-04-09 22:30:06 -0500

    Finish writing class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,6 +1,36 @@
 # Design a class: 
 #Disney Princess
-	# attributes: age, animal_friend, kingdom
+class Disney_princess
+	# attributes: name, age, animal_friend, kingdom
+	attr_reader :age
+	attr_accessor :animal_friend, :kingdom
 	# methods: sing(song), attack_or_defend(weapon), call_for_help(who)
 	
+	def initialize(name)
+
+
+	def sing(song)
+		if song.chr == (a..l)
+			puts "laaaaaaa"
+		else
+			puts "do ray me fa so la tee do!"
+		end
+	end
+
+	def attack_or_defend(weapon)
+		puts "#{name} uses #{weapon}!"
+	end
+
+	def call_for_help(who)
+		puts "#{name} calls #{who} for help!"
+	end
+end
+
+
+
+
+
+
+
+
 
