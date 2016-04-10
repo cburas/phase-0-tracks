007a72e62447babd80aa26b71f0d270d7c19daa7
Courtney Buras <cburas2311@gmail.com>
2016-04-09 22:48:47 -0500

    Use class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -2,12 +2,16 @@
 #Disney Princess
 class Disney_princess
 	# attributes: name, age, animal_friend, kingdom
-	attr_reader :age
-	attr_accessor :animal_friend, :kingdom
+	attr_reader :age, :kingdom
+	attr_accessor :animal_friend, :name
 	# methods: sing(song), attack_or_defend(weapon), call_for_help(who)
 	
 	def initialize(name)
-
+		@name
+		@age
+		@animal_friend
+		@kingdom
+	end
 
 	def sing(song)
 		if song.chr == (a..l)
@@ -24,7 +28,21 @@ class Disney_princess
 	def call_for_help(who)
 		puts "#{name} calls #{who} for help!"
 	end
+
 end
+
+dp1 = []
+
+puts "What's the name of your Disney princess?"
+name = gets.chomp
+
+d_p1 = Disney_princess.new(name)
+	d_p1.name == name
+
+p d_p1
+
+ 
+
 
 
 
