e6a88edb11cb49ebf52e976ef1116cabd5e362b3
Courtney Buras <cburas2311@gmail.com>
2016-04-12 14:25:17 -0500

    Add module

---
+++ ruby/shout.rb
@@ -0,0 +1,15 @@
+module Shout
+
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	# def self.yelling_happily(words)
+	# 	words + "!!!" + " :D"
+	# end
+	
+end
+
+# Shout.yell_angrily("Shut Up")
+
+# Shout.yelling_happily("Happy Birthday")
