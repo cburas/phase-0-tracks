e4bd835bdd6dba806ee8f5a6df1938de1996b62d
Courtney Buras <cburas2311@gmail.com>
2016-04-23 15:13:17 -0500

    Use each and map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,35 @@ end
 
 happy {puts "Thats great, me too."}
 
+animal_sounds = {
+  "dog" => "woof",
+  "cat" => "meow",
+  "pig" => "oink",
+  "horse" => "neigh"
+  }
+
+animals = ["dog", "cat", "pig", "horse"]
+
+p animal_sounds
+
+animal_sounds.each do |animal, sound| 
+  puts "The #{animal} goes #{sound}!"
+end
+
+p animal_sounds
+
+p animals
+
+animals.each do |animal|
+  puts "#{animal} is a good pet"
+end
+
+p animals
+
+map!
+
+
+animals.map! do |animals|
+  animals.capitalize
+end
+p animals
