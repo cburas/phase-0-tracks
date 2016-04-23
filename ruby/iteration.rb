d048dd497f9adce157081fed740e4a02c6c1fc66
Courtney Buras <cburas2311@gmail.com>
2016-04-23 16:01:05 -0500

    Use researched methods for array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -30,10 +30,39 @@ end
 
 p animals
 
-map!
-
+# map!
 
 animals.map! do |animals|
   animals.capitalize
 end
 p animals
+
+# release 2
+
+digits_and_words = {
+  1 => "one",
+  2 => "two",
+  3 => "three",
+  4 => "four"
+}
+
+integers = [1, 2, 3, 4]
+
+integers.delete_if {|int| int < 2}
+p integers
+
+digits_and_words.delete_if {|digit, word| digit < 2}
+p digits_and_words
+
+integers.keep_if{|int| int > 2}
+p integers
+
+digits_and_words.keep_if{|digit, word| digit > 2}
+p digits_and_words
+
+integers.collect!{|int| int + 100}
+p integers
+
+digits_and_words.reject!{|digit, word| digit > 3}
+p digits_and_words
+
