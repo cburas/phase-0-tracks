a57cbb3e0598e82b4c5fda0aeec8ad4aeb2ec0f0
Courtney Buras <cburas2311@gmail.com>
2016-04-26 22:24:53 -0500

    Add reflection

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -27,13 +27,14 @@ def remove_item(list, item)
   list
 end
 
+# Commented out for reduncancy with add_item
 # Method to update the quantity of an item
-def update(list, item, quantity)
+# def update(list, item, quantity)
 # input: what's item and quantity.
-  list[item] = quantity
+  # list[item] = quantity
 # output: print new hash with updated quantity
-  list
-end
+  # list
+# end
 
 # Method to print a list and make it look pretty
 def print_pretty(list)
@@ -42,16 +43,28 @@ def print_pretty(list)
     puts "You need #{quantity} of #{food}."
   end
 # output: printed formatted hash
+	list
 end
 
-grocery_list = create_list("carrots cereal cookies")
-p grocery_list
+#p grocery_list
+
+#p add_item(grocery_list, "apples", 4)
+#
+#p remove_item(grocery_list, "cereal")
+#
+#p update(grocery_list, "cookies", 3)
+#
+# print_pretty(grocery_list)
 
 
-p add_item(grocery_list, "apples", 4)
-
-p remove_item(grocery_list, "cereal")
-
-p update(grocery_list, "cookies", 3)
-
-print_pretty(grocery_list)
+# 	I learned pseudocode can be great for thinking things through and 
+# figuring out what you actually want as an output.
+# 	Um, trade-offs? I suppose that it doesn't look nice?
+# 	A method returns the last line of code which is why you see list
+# repeated constantly at the end of our methods.
+# 	You can pass just about everything through a method, including
+# arrays, hashes, blocks, integers, strings, variables, etc.
+# 	You pass information between methods through parameters/arguments.
+# 	It came back to me about how you need arguments to share info
+# between methods. I had also forgotten how simple it was to add and
+# update information into a hash.
