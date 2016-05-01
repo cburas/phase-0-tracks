e04ca99bec5f723eccc147245c80113e262993ac
Courtney Buras <cburas2311@gmail.com>
2016-05-01 08:24:56 -0500

    Create Santa class and basic methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,58 +2,17 @@
 
 class Santa
 
-	attr_accessor :reindeer_ranking, :age, :gender, :ethnicity
-
-	def initialize(gender, ethnicity) 
-		@gender  
-		@ethnicity  
-		@age = 0  
-		@reindeer_ranking =["Rudolph","Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]  
+	def initialize
+		p "initializing a Santa instance..."
 	end
 
-	def speak 
-		print "Ho, Ho, ho! Haaaaappy holidays!" 
+	def speak
+		p "Ho, ho, ho! Haaaapppy Holidays!"
 	end
 
-	def eat_milk_and_cookies(cookie) 
-		print "That was a good #{cookie}!" 
+	def eat_milk_and_cookies(cookie)
+		p "That was a good #{cookie}!"
 	end
+
+
 end
-
-#Add some diverse initializations to file as driver code. 
-santas = []
-example_genders = ["agender", "female", "male", "transfemale", "transmale", "bigender", "gender fluid", "N/A"]
-
-example_ethnicities = ["white", "black", "Asian", "Indian", "Latino", "prefer not to say", "African", "Middle Eastern"]
-
-example_genders.length.times do |i|
-		santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
-
-<<<<<<< HEAD
-=======
-print santas
-
-example_ages = (1..140).to_a
-
->>>>>>> practice
-
-# 20.times do |i|
-# Santa.new(example_genders.sample , example_ethnicities.sample)
-# "Santa #{i}"
-# end
-
-i = 0
-while i <= 20
-<<<<<<< HEAD
-	Santa.new(example_ethnicities.sample, example_genders.sample)
-=======
-	s = Santa.new(example_ethnicities.sample, example_genders.sample)
-	s.age = example_ages.sample
->>>>>>> practice
-	i += 1
-	p s
-end
-
-
-
