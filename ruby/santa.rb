ae1436d4842a885e951efc3e43d2ca5ba72303fe
Courtney Buras <cburas2311@gmail.com>
2016-05-01 17:34:42 -0500

    Add instance variables

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,9 +2,19 @@
 
 class Santa
 
-	def initialize
-		p "initializing a Santa instance..."
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
+
+	santas = []
+	example_genders = ["agender", "female", "bigender", "male", "gender fluid", "transgender", "N/A"]
+	example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature", "Indian", "N/A"]
+	example_genders.length.times do |i|
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
+	end	
 
 	def speak
 		p "Ho, ho, ho! Haaaapppy Holidays!"
@@ -23,4 +33,11 @@ end
 # Nick.speak
 
 # Nick.eat_milk_and_cookies("gingerbread cookie")
+
+Whoopi = Santa.new("female", "black")
+
+Grover = Santa.new("agender", "Mystical Creature")
+
+Joey = Santa.new("male", "white")
+
 
