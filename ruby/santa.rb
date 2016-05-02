3ba2dc1dd75da951fc3157131789b9975763ae11
Courtney Buras <cburas2311@gmail.com>
2016-05-01 19:53:08 -0500

    Create ten santas. Can't change age yet

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -9,13 +9,6 @@ class Santa
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
-	end
-
-	santas = []
-	example_genders = ["agender", "female", "bigender", "male", "gender fluid", "transgender", "N/A"]
-	example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature", "Indian", "N/A"]
-	example_genders.length.times do |i|
-	santas << Santa.new(example_genders[i], example_ethnicities[i])
 	end	
 
 	def speak
@@ -28,7 +21,7 @@ class Santa
 
 	def celebrate_birthday
 		#age one year
-		@age + 1
+		@age= @age + 1
 		p "Happy Birthday Santa! You're #{@age} now!"
 	end
 
@@ -43,6 +36,13 @@ class Santa
 
 end
 
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "gender fluid", "transgender", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature", "Indian", "N/A"]
+example_genders.length.times do |i|
+santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
 
 # Nick = Santa.new
 # Nick.speak
@@ -51,10 +51,20 @@ end
 
 # Whoopi = Santa.new("female", "black")
 # Grover = Santa.new("agender", "Mystical Creature")
-Joey = Santa.new("male", "white")
+# Joey = Santa.new("male", "white")
 
-Joey.get_mad_at("Vixen")
+# Joey.celebrate_birthday
+# Joey.celebrate_birthday
 
 
-# 50.times { |claus| claus = Santa.new }
+10.times { p Santa.new(example_genders.sample, example_ethnicities.sample) 
+}
+
+# couldn't figure out how to use .rand(140). How can I call it on something that doesn't have a name? And when I do give the whole thing a name, the program thinks it's only equal to the integer...!
+
+
+
+
+
+
 
