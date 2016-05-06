d2861de6767026abb143200cfe1d18e161f61c76
Courtney Buras <cburas2311@gmail.com>
2016-05-05 19:57:47 -0500

    Add beautiful function with comments

--- js/explore.js
+++ js/explore.js
@@ -6,13 +6,35 @@ var word = "javascript"
 // I know I'm going to need it to print on the screen so I'm going to need, console and 'return'
 
 function reverseString(string) {
-	// I will probably need a loop to go through each character.. but how to move them??
-
+	return string === '' ? '' : reverseString(string.slice(1)) + string[0];
 }
-
 
 // I know I will need to call the function
 console.log(reverseString(word));
+
+// I have no idea how that works.
+// I found it after passing over a bunch of super long answers
+// I tried the split, reverse, join answer but it kept the commas.
+
+// '===' is the same as '==' but no type conversion is done...
+// ok that's good because it has to be reversed..
+// ok, so apparently it means 'both operands reference the same object'
+
+// OO! OO! Found it!!!
+
+// 'condition' ? 'value-if-true' : 'value-if-false'
+// '?' being equal to 'then' and ':' equal to 'else'
+
+// but what about the spaces? oh! so.. if string refers to nothing then return nothing
+// else.. return the string sliced.. by one.. and then string's first letter??
+
+// I'm printing this out.
+
+console.log((word.slice(1)) + word[0])
+
+// i get 'avascriptj' ... i seriously have no idea how this works..
+// but it does and it's beautiful!
+
 
 
 
