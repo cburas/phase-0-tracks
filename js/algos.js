2fa6a3e7685929005ef6b5f6e5f8d9d9b68a2c31
Courtney Buras <cburas2311@gmail.com>
2016-05-06 18:00:18 -0500

    Begin release 1

--- js/algos.js
+++ js/algos.js
@@ -33,4 +33,14 @@ function jsLength(array){
 
 console.log(jsLength(words));
 
-// for the record, this was not fun.
+// for the record, this was not fun. And I'm not done...
+
+// {name: "Steven", age: 54} vs {name: "Tamir", age: 54} has to eq true
+// comparing means using && or || 
+
+function compareObjects(firstPiece, secondPiece){
+
+}
+
+
+
