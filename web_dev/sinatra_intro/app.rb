c208a41c6a4beceda4e81dcca38d93765a8a458d
Courtney Buras <cburas2311@gmail.com>
2016-05-21 17:09:58 -0500

    Create addition parameter

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -8,42 +8,42 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
-get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
-end
+# get '/' do
+#   "#{params[:name]} is #{params[:age]} years old."
+# end
 
-# write a GET route with
-# route parameters
-get '/about/:person' do
-  person = params[:person]
-  "#{person} is a programmer, and #{person} is learning Sinatra."
-end
+# # write a GET route with
+# # route parameters
+# get '/about/:person' do
+#   person = params[:person]
+#   "#{person} is a programmer, and #{person} is learning Sinatra."
+# end
 
-get '/:person_1/loves/:person_2' do
-  "#{params[:person_1]} loves #{params[:person_2]}"
-end
+# get '/:person_1/loves/:person_2' do
+#   "#{params[:person_1]} loves #{params[:person_2]}"
+# end
 
-# write a GET route that retrieves
-# all student data
-get '/students' do
-  students = db.execute("SELECT * FROM students")
-  response = ""
-  students.each do |student|
-    response << "ID: #{student['id']}<br>"
-    response << "Name: #{student['name']}<br>"
-    response << "Age: #{student['age']}<br>"
-    response << "Campus: #{student['campus']}<br><br>"
-  end
-  response
-end
+# # write a GET route that retrieves
+# # all student data
+# get '/students' do
+#   students = db.execute("SELECT * FROM students")
+#   response = ""
+#   students.each do |student|
+#     response << "ID: #{student['id']}<br>"
+#     response << "Name: #{student['name']}<br>"
+#     response << "Age: #{student['age']}<br>"
+#     response << "Campus: #{student['campus']}<br><br>"
+#   end
+#   response
+# end
 
-# write a GET route that retrieves
-# a particular student
+# # write a GET route that retrieves
+# # a particular student
 
-get '/students/:id' do
-  student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
-  student.to_s
-end
+# get '/students/:id' do
+#   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+#   student.to_s
+# end
 
 get '/contact' do
 	"2812 Greenport Ave.<br>Temple, ID 703542<br>"
@@ -58,5 +58,12 @@ get '/great_job' do
 	end
 end
 
+get '/add' do
+	# num1 = params[num1]
+	# num2 = params[num2]
+	answer = 1 + 2
+	answer.to_s
+end
 
+# how the hell do you use integers???!!!!
 
